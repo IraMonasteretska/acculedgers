0b3ee958c0d94a4c2fe8135ac6158b9d2619bdfe
IraMonasteretska <44293548+IraMonasteretska@users.noreply.github.com>
2024-01-15 15:59:29 +0200

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -32,7 +32,8 @@ $(document).ready(function () {
         paddingContainer = ((windowWidth - containerWidth) / 2)
     $('.outcustservproc__leftsect').css('padding-left', paddingContainer);
     $('.outcustservproc__rightsect').css('padding-right', paddingContainer);
-
+    // serv
+    $('.pagetitlesection__descr, .services-wrapper').css('padding-left', paddingContainer);
 
     $(window).resize(function () {
         var windowWidth = $(document).width(),
@@ -40,6 +41,9 @@ $(document).ready(function () {
             paddingContainer = ((windowWidth - containerWidth) / 2)
         $('.outcustservproc__leftsect').css('padding-left', paddingContainer);
         $('.outcustservproc__rightsect').css('padding-right', paddingContainer);
+
+        // serv
+        $('.pagetitlesection__descr, .services-wrapper').css('padding-left', paddingContainer);
     });
 
 
@@ -56,6 +60,44 @@ $(document).ready(function () {
 
 
 
+    
+            if ($('.servsidebar__servlist-body').length > 0) {
+    
+                $(".servsidebar__servlist-body ul").on("click", "a", function (event) {
+                    console.log('sdsdsd')
+                    event.preventDefault();
+                    var id = $(this).attr('href'),
+                        top = $(id).offset().top;
+                    $('body,html').animate({ scrollTop: top - 105 }, 1500);
+                });
+    
+    
+                const links = document.querySelectorAll('.servsidebar__servlist-body li a');
+                window.addEventListener('scroll', () => {
+                    const anchors = document.querySelectorAll('.servsect [id]');
+                    anchors.forEach(anchor => {
+                        if (anchor.closest('.servsect')) {
+                            const anchorTop = anchor.getBoundingClientRect().top + window.pageYOffset;
+                            if (window.pageYOffset >= anchorTop - 110) {
+                                links.forEach(link => {
+                                    const correspondingLink = document.querySelector(`.servsidebar__servlist-body li a[href="#${anchor.id}"]`);
+                                    if (correspondingLink) {
+                                        link.classList.remove('active');
+                                        correspondingLink.classList.add('active');
+                                    }
+                                });
+                            }
+                        }
+                    });
+                });
+    
+    
+    
+    
+    
+    
+    
+            }
 
 
 
